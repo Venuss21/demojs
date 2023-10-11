1c93643b9ad1edfb77b573217b3af1666068c2a5
Nguyen Hoai <nghoaii21@gmail.com>
2023-10-11 21:52:55 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const username = document.querySelector("input[name='username']").value;
+  const password = document.querySelector("input[name='password']").value;
+
+  // Kiểm tra xem dữ liệu có hợp lệ hay không
+  if (username === "" || password === "") {
+    // Nếu dữ liệu không hợp lệ, hiển thị thông báo lỗi
+    alert("Vui lòng nhập tên đăng nhập và mật khẩu.");
+  } else {
+    // Nếu dữ liệu hợp lệ, gửi dữ liệu lên server
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Nếu đăng nhập thành công, chuyển hướng người dùng đến trang chủ
+        if (data.success) {
+          window.location.href = "/";
+        } else {
+          // Nếu đăng nhập thất bại, hiển thị thông báo lỗi
+          alert(data.message);
+        }
+      });
+  }
 }
 
 export default App;
